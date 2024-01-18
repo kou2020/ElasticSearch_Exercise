7f86f13a74ee65b4798b71a48e4739136890b4c7
kou2020 <shangguangchunxue@gmail.com>
2024-01-18 11:55:12 +0900

    Doc_Query_2

--- es-test/src/main/java/com/atguigu/es/test/ESTest_Doc_Query.java
+++ es-test/src/main/java/com/atguigu/es/test/ESTest_Doc_Query.java
@@ -8,11 +8,14 @@ import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
-import org.elasticsearch.index.query.QueryBuilder;
-import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.common.unit.Fuzziness;
+import org.elasticsearch.index.query.*;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.SearchHits;
+import org.elasticsearch.search.aggregations.AggregationBuilder;
+import org.elasticsearch.search.aggregations.AggregationBuilders;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightBuilder;
 import org.elasticsearch.search.sort.SortOrder;
 
 import java.io.IOException;
@@ -47,13 +50,60 @@ public class ESTest_Doc_Query {
 //        request.source(builder);
 
         // 5.过滤字段
-        SearchSourceBuilder builder = new SearchSourceBuilder().query(QueryBuilders.matchAllQuery());
-        String[] excludes ={"age"};
-        String[] includes ={};
-        builder.fetchSource(includes,excludes);
+//        SearchSourceBuilder builder = new SearchSourceBuilder().query(QueryBuilders.matchAllQuery());
+//        String[] excludes ={"age"};
+//        String[] includes ={};
+//        builder.fetchSource(includes,excludes);
+//        request.source(builder);
+
+        // 6.组合查询((must=and/mustNot=not and)/(should=or))
+//        SearchSourceBuilder builder = new SearchSourceBuilder();
+//        BoolQueryBuilder boolQueryBuilder = QueryBuilders.boolQuery();
+////        boolQueryBuilder.must(QueryBuilders.matchQuery("age",30));
+////        boolQueryBuilder.must(QueryBuilders.matchQuery("sex","男"));
+////        boolQueryBuilder.mustNot(QueryBuilders.matchQuery("sex","男"));
+//        boolQueryBuilder.should(QueryBuilders.matchQuery("age",30));
+//        boolQueryBuilder.should(QueryBuilders.matchQuery("age",40));
+//        builder.query(boolQueryBuilder);
+
+        // 7.范围查询
+//        SearchSourceBuilder builder = new SearchSourceBuilder();
+//        RangeQueryBuilder rangeQuery = QueryBuilders.rangeQuery("age");
+//        rangeQuery.gte(30);
+//        rangeQuery.lte(40);
+//        builder.query(rangeQuery);
+
+        // 8.模糊查询
+//        SearchSourceBuilder builder = new SearchSourceBuilder();
+//        // 模糊查询姓名为wangwu的人，名字相差一个字母或者不相差
+//        // 通过设置Fuzziness的属性设置偏差值
+//        builder.query(QueryBuilders.fuzzyQuery("name","wangwu").fuzziness(Fuzziness.ONE));
+
+        // 9.高亮查询
+//        SearchSourceBuilder builder = new SearchSourceBuilder();
+//        TermQueryBuilder termQueryBuilder = QueryBuilders.termQuery("name", "zhangsan");
+//        // 使用高亮
+//        HighlightBuilder highlightBuilder = new HighlightBuilder();
+//        // 设置高亮
+//        highlightBuilder.preTags("<font color='red'>");   //前缀标签
+//        highlightBuilder.postTags("</font");            // 后缀标签
+//        highlightBuilder.field("name");           //指定高亮显示的名称
+//        builder.highlighter(highlightBuilder);
+//        builder.query(termQueryBuilder);
+
+        // 10.聚合查询
+//        SearchSourceBuilder builder = new SearchSourceBuilder();
+//        AggregationBuilder aggregationBuilder = AggregationBuilders.max("maxAge").field("age");
+//        builder.aggregation(aggregationBuilder);
+
+        // 11.分组查询(根据年龄分组)
+        SearchSourceBuilder builder = new SearchSourceBuilder();
+        AggregationBuilder aggregationBuilder = AggregationBuilders.terms("ageGroup").field("age");
+        builder.aggregation(aggregationBuilder);
+
+
+
         request.source(builder);
-
-
         SearchResponse response = esClient.search(request, RequestOptions.DEFAULT);
         SearchHits hits = response.getHits();
         // 响应状态
